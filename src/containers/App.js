d3cfc7ab7b15b5f88d01641fda932f108513bca7
Navin <navinkumar247@gmail.com>
2020-07-29 22:51:00 +0530

    React and redux

---
+++ src/containers/App.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+// import {robots} from './robots';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import './App.css';
+import Scroll from '../components/Scroll';
+import { setSearchField, requestRobots } from '../actions';
+
+const mapStateToProps = state=> {
+    return {
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
+    }
+}
+
+class App extends Component {
+    
+    componentDidMount() {
+       this.props.onRequestRobots();
+    }
+
+
+    render() {
+        const { searchField, onSearchChange, robots, isPending } = this.props;
+        const filterRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
+        });
+        return isPending ? (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={onSearchChange}/>
+                    <p>Loading</p>
+                </div>
+                
+            ):(
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={onSearchChange}/>
+                    <Scroll>
+                        <CardList robots={filterRobots}/>    
+                    </Scroll>
+                    
+                </div>
+                
+            );
+    }
+} 
+    
+export default connect(mapStateToProps, mapDispatchToProps)(App);
